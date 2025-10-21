d9c897eb7cbb5dae28fd514bd6e320ef660f5c57
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-10-21 15:27:58 +0300

    comment #conflict_inna

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -1,6 +1,6 @@
 from selene import browser, be, have
 
-# branch
+# branch_conflict_inna
 browser.open('https://google.com')
 browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
 browser.element('html').should(have.text('About this page'))
